05b43c77fe212568261c2132bdae8c2dd400d6f5
Man <nguyenman025@gmail.com>
2025-03-24 13:42:15 -0400

    boiler plate set up for webpack template

---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+module.exports = merge(common, {
+  mode: 'development',
+  output: {
+    filename: 'main.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 9000,
+    static: './dist'
+  }
+})
